0cb8e8d8a1e06fc4cce0b5227f825d368bc8f017
Adina Singer <adinasinger@gmail.com>
2016-08-21 14:50:03 -0700

    main fxn opts 1-4

--- shopdict_parse.py
+++ shopdict_parse.py
@@ -16,13 +16,14 @@ shop_dict= {"Target": ["socks", "soap", "detergent", "sponges"], "Safeway": ["bu
 
 def menu_prompt():
     # todo: display menu and get choice using raw_input from user.
-    prompt = int(raw_input("0 - Main Menu\n1 - Show all lists.\n2 - Show a specific list. \n3 - Add a new shopping list.\n4 - Add an item to a shopping list.\n5 - Remove an item from a shopping list.\n6 - Remove a list by nickname.\n7 - Exit when you are done."))
+    prompt = int(raw_input("0 - Main Menu\n1 - Show all lists.\n2 - Show a specific list. \n3 - Add a new shopping list.\n4 - Add an item to a shopping list.\n5 - Remove an item from a shopping list.\n6 - Remove a list by nickname.\n7 - Exit when you are done.\n"))
     return prompt
 
 
 def show_all_lists():
     # todo: display all key:value pairs in dictionary
-    print shop_dict
+    return shop_dict.items()
+
 
 
 def show_sorted_list(key):
@@ -52,18 +53,21 @@ def add_item_to_list(key, item):
     # add an item to an existing list
    
     # check to see key exists
+    # if item exists, let user know
+    # then add item to list
+
     if key not in shop_dict:
     	print "You haven't created a list for this store. Choose option 3 on main menu."
     elif item in shop_dict[key]:
     	print "You already have this item on your list for %s." % (key)
     else:
     	shop_dict[key] = shop_dict[key] + [item]
-	print shop_dict[key]
-    # if yes, check to see item does not exist
-    # then add item to list
+	return show_sorted_list(shop_dict[key])
+   
+   
 
 
-    # make sure the item doesn't already exist
+    
     # deal with uppercase/lowercase
     
 
@@ -81,22 +85,34 @@ def remove_list(key):
 def main():
     # this is where the core logic will be
     # start by calling the menu prompt to return the user's choice
-#    menu_prompt()
- #   show_all_lists()
- #if option 2 entered
- # key_name = raw_input("Which store list do you want?")
- # show_sorted_list(key_name)
+    prompt = menu_prompt()
+    if prompt == 1:
+        print show_all_lists()
+        print "\n"
+        menu_prompt()
+ #if option 2 entered   
+    elif prompt == 2:
+        key_name = raw_input("Which store list do you want? ")
+        show_sorted_list(key_name)
+        print "\n"
+        menu_prompt()
  # if option 3 entered
- # new_key = raw_input("What store do you want to add a shopping list for?")
- # add_new_list(new_key)
+    elif prompt == 3:
+        new_key = raw_input("What store do you want to add a shopping list for? ")
+        add_new_list(new_key)
+        print "\n"
+        menu_prompt()
  # if option 4 entered
-  # key_name4 = raw_input("What store list do you want to add your item to? ")
-  # add_item = raw_input("What item do you want to add? ")
-  # add_item_to_list(key_name4,add_item)
-# testing adding multiple parsed items
-
-    add_mult_items = raw_input("Which items do you want to add? (Use a comma to separate) ")
-    print parse_items(add_mult_items)
+    elif prompt == 4:
+        key_name4 = raw_input("What store list do you want to add your item to? ")
+        add_item = raw_input("What item do you want to add? ")
+        add_item_to_list(key_name4,add_item)
+        print "\n"
+        menu_prompt()    
+# testing adding multiple parsed items   
+    else:
+        add_mult_items = raw_input("Which items do you want to add? (Use a comma to separate) ")
+        print parse_items(add_mult_items)
 
 
 if __name__ == "__main__":
